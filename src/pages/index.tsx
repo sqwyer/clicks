3b51d1e7f0883542681cc10b8182e5f155d4eba9
Sawyer Bivens <570000826@nlrsd.org>
2022-12-15 11:33:03 -0600

    Change io import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import { useState } from "react";
 import { useEffect } from 'react'
-import io from 'socket.io-client'
+import io from 'socket.io-client/dist/socket.io'
 let socket: any;
 
 const Home: NextPage = () => {
