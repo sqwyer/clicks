386a199b739fe9699ccddfd8dd718bb33968dea1
Sawyer Bivens <570000826@nlrsd.org>
2022-12-15 11:34:37 -0600

    Revert io import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import { useState } from "react";
 import { useEffect } from 'react'
-import io from 'socket.io-client/dist/socket.io'
+import io from 'socket.io-client'
 let socket: any;
 
 const Home: NextPage = () => {
